6d1b6681efa03c5f7b413b18999378f9ed4f0fe0
Dylan Aumond <dylanaumond@gmail.com>
2021-11-22 16:58:00 +0100

    Ajout de client

---
+++ CleanIt/src/Controller/ClientController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Client;
+use App\Form\ClientType;
+use Symfony\Component\HttpFoundation\Request;
+
+class ClientController extends AbstractController
+{
+    #[Route('/client', name: 'client')]
+    public function index(): Response
+    {
+        return $this->render('client/index.html.twig', [
+            'controller_name' => 'ClientController',
+        ]);
+    }
+    public function ajouterClient(Request $request){
+        $client = new Client();
+        $form = $this->createForm(ClientType::class, $client);
+        $form->handleRequest($request);{}
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+    
+                $client = $form->getData();
+    
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($client);
+                $entityManager->flush();
+    
+            return $this->render('client/consulter.html.twig', ['client' => $client,]);
+        }
+        else
+            {
+                return $this->render('client/ajouter.html.twig', array('form' => $form->createView(),));
+        }
+    }
+        public function consulterClient($id){
+            $client = $this->getDoctrine()
+                ->getRepository(Client::class)
+                ->find($id);
+        
+                if (!$client) {
+                    throw $this->createNotFoundException(
+                    'Aucun client trouvé avec le numéro '.$id
+                    );
+                }
+        
+                return $this->render('client/consulter.html.twig', [
+                    'client' => $client,]);
+        }
+        public function listerClient(){
+            $repository = $this->getDoctrine()->getRepository(Client::class);
+            $client = $repository->findAll();
+            return $this->render('client/lister.html.twig', [
+                'pClient' => $client,]);	
+        }
+}
