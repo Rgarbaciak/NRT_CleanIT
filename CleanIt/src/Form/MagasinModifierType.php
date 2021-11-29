c4c7628ab7f827723f58bf25955166c457287611
Robin Garbaciak <robin.garbaciak@gmail.com>
2021-11-29 10:16:57 +0100

    Ajout ModifierMagasin MaximeB

---
+++ CleanIt/src/Form/MagasinModifierType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Magasin;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class MagasinModifierType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('libelle')
+            ->add('rue')
+            ->add('ville')
+            ->add('copos')
+            ->add('pays')
+            ->add('horaires')
+            
+            ->add('enseigne',EntityType::class, array('class' => 'App\Entity\Enseigne','choice_label' => 'libelle' )) 
+            ->add('enregistrer', SubmitType::class, array('label' => 'modifier le magasin'))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Magasin::class,
+        ]);
+    }
+}
